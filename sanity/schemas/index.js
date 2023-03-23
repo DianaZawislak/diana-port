a95aeed6acdbfc0329bc62583818ac1e7a851e0a
Diana Zawislak <diana.zawislak@icloud.com>
2023-03-23 13:58:29 -0400

    sanity schemas config

---
+++ sanity/schemas/index.js
@@ -0,0 +1,8 @@
+import experience from './experience'
+import pageInfo from './pageInfo'
+import project from './project'
+import skill from './skill'
+import social from './social'
+import education from './education'
+
+export const schemaTypes = [education, experience, pageInfo, project, skill, social]
