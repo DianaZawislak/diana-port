12ffb765220e8436468f5cfcf08814347e347446
Diana Zawislak <diana.zawislak@icloud.com>
2023-03-23 16:43:00 -0400

    api config change

--- pages/api/getPageInfo.ts
+++ pages/api/getPageInfo.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { groq } from "next-sanity";
 import { sanityClient } from "../../sanity";
@@ -16,7 +15,21 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  // Set the CORS headers
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Authorization, Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  // Call the Sanity API
   const pageInfo: PageInfo = await sanityClient.fetch(pageInfoQuery);
 
+  // Return the response
   res.status(200).json({ pageInfo });
 }
