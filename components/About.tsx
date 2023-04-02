dd3b6b711ad49f0d3fd3c8999cfc8bbcbd5740ef
Diana Zawislak <diana.zawislak@icloud.com>
2023-04-02 18:22:14 -0400

    minor fix

--- components/About.tsx
+++ components/About.tsx
@@ -19,7 +19,6 @@ function About({ pageInfo }: Props) {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500  text-2xl lg:text-4xl md:text-3xl  sm:text-2xl">
         About
       </h3>
-
    <motion.img
         initial={{
           x: -200,
