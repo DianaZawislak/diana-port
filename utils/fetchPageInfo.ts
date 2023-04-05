369f0d47ebc7f9736c595697c15cda2ccc1737b6
Diana Zawislak <diana.zawislak@icloud.com>
2023-04-04 22:15:41 -0400

    deployment variables

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -3,7 +3,7 @@ import { PageInfo } from "../typings";
 export const fetchPageInfo = async () => {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
+      `${process.env.aportfolio}.herokuapp/api/getPageInfo`
     );
 
     // Check res for error
