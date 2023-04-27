342c7a37495f6d44f8c78e9f9860a95e4a287067
Diana Zawislak <diana.zawislak@icloud.com>
2023-04-27 12:40:57 -0400

    styles

--- components/Education.tsx
+++ components/Education.tsx
@@ -26,39 +26,86 @@ function Education({ school }: Props) {
   };
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{ duration: 1.5 }}
-      className="flex relative overflow-hidden flex-col text-center md:flex-row max-w-full px-5 font-thin h-screen justify-evenly mx-auto items-center"
-    >
-      <h3 className="absolute top-28 uppercase tracking-[20px] text-gray-500 text-2xl lg:text-4xl md:text-3xl sm:text-2xl">
-        Education
-      </h3>
+    <>
+      <style>
+        {`
+          .education-card-image {
+            width: 100%;
+            height: auto;
+          }
 
-      <button
-        className="absolute left-5 z-10 h-full w-12 flex items-center custom-arrow-color justify-center text-8xl text-#7bff00"
-        onClick={scrollLeft}
-      >
-        &lsaquo;
-      </button>
+          @media (max-width: 1024px) {
+            .education-heading {
+              top: 16px !important;
+              font-size: 2.5rem !important;
+            }
 
-      <div
-        ref={scrollRef}
-        className="w-full flex space-x-5 gap-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#7bff00]/40"
-      >
-        {school?.reverse().map((education) => (
-          <EducationCard key={education._id} education={education} />
-        ))}
-      </div>
+            .arrow-button {
+              width: 10rem !important;
+              font-size: 6rem !important;
+            }
+          }
 
-      <button
-        className="absolute right-5 z-10 h-full w-12 flex items-center justify-center text-8xl custom-arrow-color"
-        onClick={scrollRight}
+          @media (max-width: 768px) {
+            .education-heading {
+              top: 12px !important;
+              font-size: 2rem !important;
+            }
+
+            .arrow-button {
+              width: 8rem !important;
+              font-size: 5rem !important;
+            }
+          }
+
+          @media (max-width: 480px) {
+            .education-heading {
+              top: 8px !important;
+              font-size: 1.5rem !important;
+            }
+
+            .arrow-button {
+              width: 6rem !important;
+              font-size: 4rem !important;
+            }
+          }
+        `}
+      </style>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        className="flex relative overflow-hidden flex-col text-center md:flex-row max-w-full px-5 font-thin h-screen justify-evenly mx-auto items-center"
       >
-        &rsaquo;
-      </button>
-    </motion.div>
+   <h3 className="absolute top-[100px] sm:top-[80px] md:top-[60px] lg:top-[40px] uppercase tracking-[20px] text-gray-500 text-2xl lg:text-4xl md:text-3xl sm:text-2xl">
+  Education
+</h3>
+
+
+        <button
+          className="arrow-button absolute left-5 z-10 h-full w-12 flex items-center custom-arrow-color justify-center text-8xl text-#7bff00"
+          onClick={scrollLeft}
+        >
+          &lsaquo;
+        </button>
+
+        <div
+          ref={scrollRef}
+          className="w-full flex space-x-5 gap-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#7bff00]/40"
+        >
+          {school?.reverse().map((education) => (
+            <EducationCard key={education._id} education={education} />
+          ))}
+        </div>
+
+        <button
+          className="arrow-button absolute right-5 z-10 h-full w-12 flex items-center justify-center text-8xl custom-arrow-color"
+          onClick={scrollRight}
+        >
+          &rsaquo;
+        </button>
+      </motion.div>
+    </>
   );
 }
 
--- components/EducationCard.tsx
+++ components/EducationCard.tsx
@@ -9,8 +9,41 @@ type Props = {
 
 function EducationCard({ education }: Props) {
   return (
-    <article className="flex flex-col rounded-lg items-center mt-5 space-y-7 flex-shrink-0  xl:w-[900px] xl-h[800px] snap-center bg-[#252323] 
-    p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-scroll lg:overflow-hidden" style={{ margin: '0 auto' }}>
+    <article
+      className="flex flex-col rounded-lg items-center mt-5 space-y-7 flex-shrink-0 xl:w-[900px] xl-h[800px] snap-center bg-[#252323]
+      p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-scroll lg:overflow-hidden"
+      style={{ margin: "0 auto" }}
+    >
+      <style>
+        {`
+        .education-card-image {
+          width: 100%;
+          height: auto;
+          max-height: 40vh;
+        }
+
+        @media (max-width: 1024px) {
+          .education-card-image {
+            width: 80%;
+            max-height: 35vh;
+          }
+        }
+
+        @media (max-width: 768px) {
+          .education-card-image {
+            width: 60%;
+            max-height: 30vh;
+          }
+        }
+
+        @media (max-width: 480px) {
+          .education-card-image {
+            width: 100%;
+            max-height: 25vh;
+          }
+        }
+      `}
+      </style>
       <motion.img
         initial={{
           y: -100,
@@ -20,23 +53,17 @@ function EducationCard({ education }: Props) {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         src={urlFor(education.diplomaImage).url()}
-        className=" 
-                  xl:w-[700px] 
-                  object-center
-                  object-cover"
+        className="education-card-image
+          xl:w-[700px]
+          object-center
+          object-cover"
       />
 
-
-<p className="uppercase py-1 mt-1 text-gray-300">
-  {education.yearStarted ? education.yearStarted : "N/A"} - {education.dateEnded ? education.dateEnded : "N/A"}{" "}
-</p>
-
-
-
-
-
-      </article>
-
+      <p className="uppercase py-1 mt-1 text-gray-300">
+        {education.yearStarted ? education.yearStarted : "N/A"} -{" "}
+        {education.dateEnded ? education.dateEnded : "N/A"}{" "}
+      </p>
+    </article>
   );
 }
 
