acc4d6d18b299dede7e748cffd615a84cfc5a34b
Diana Zawislak <diana.zawislak@icloud.com>
2023-05-05 00:55:01 -0400

    education card styles animation

--- components/EducationCard.tsx
+++ components/EducationCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import { EducationType } from "../typings";
 import { urlFor } from "../sanity";
@@ -7,11 +7,38 @@ type Props = {
   education: EducationType;
 };
 
+function useInView() {
+  const [inView, setInView] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setInView(entry.isIntersecting);
+      },
+      { threshold: 0.5 } // Change this value
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref]);
+
+  return [ref, inView];
+}
+
 function EducationCard({ education }: Props) {
+  const [ref, inView] = useInView();
   return (
     <article
       className="flex flex-col rounded-lg items-center  mt-5 space-y-7 flex-shrink-0  h-[600px] md:h-[620px] lg:h-[600px] xl:w-[900px] xl:h-[600px] snap-center bg-[#252323]
-      p-5 hover:opacity-100 opacity-40 cursor-pointer transition-opacity small-screen-opacity duration-200 overflow-hidden
+      p-5 hover:opacity-100 opacity-70 cursor-pointer transition-opacity small-screen-opacity duration-200 overflow-hidden
       w-full sm:w-full md:w-3/4"
       style={{ margin: "0 auto" }}
     >
@@ -60,13 +87,20 @@ function EducationCard({ education }: Props) {
       `}
       </style>
       <motion.img
+        ref={ref}
         initial={{
           y: -100,
           opacity: 0,
         }}
+        animate={
+          inView
+            ? {
+                opacity: 1,
+                y: 0,
+              }
+            : {}
+        }
         transition={{ duration: 1.2 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
         src={urlFor(education.diplomaImage).url()}
         className="education-card-image
           xl:w-[700px]
@@ -74,7 +108,8 @@ function EducationCard({ education }: Props) {
           object-cover"
       />
 
-      <p className="uppercase py-1 mt-1 text-gray-300">
+      <p className="uppercase   text-gray-300">
+        {education.certificateName ? education.certificateName : "N/A"} {" "}<br />
         {education.yearStarted ? education.yearStarted : "N/A"} -{" "}
         {education.dateEnded ? education.dateEnded : "N/A"}{" "}
       </p>
