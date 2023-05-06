1c3a4b31a8be7cbbace8b52e5d706aed20e129b1
Diana Zawislak <diana.zawislak@icloud.com>
2023-05-06 15:45:39 -0400

    secure server settings

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const next = require('next');
+const { parse } = require('url');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Enforce HTTPS
+  server.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] === 'http' && !dev) {
+      res.redirect(301, `https://${req.headers.host}${req.url}`);
+    } else {
+      next();
+    }
+  });
+
+  server.all('*', (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  });
+
+  const port = process.env.PORT || 3000;
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
